677f5520010ae6d38793b79cf5dde9e63fd4962c
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-14 15:12:55 +0530

    Abstract Factory Method

---
+++ AbstractFactory/DarkTextField.java
@@ -0,0 +1,8 @@
+package AbstractFactory;
+
+public class DarkTextField implements TextField{
+    @Override
+    public void render() {
+        System.out.println("Rendering Dark Text Field");
+    }
+}
